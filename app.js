cf4bc93257fa42f1266a8d1551bd8ab37f075194
Lesthy <103683378+Lesthy@users.noreply.github.com>
2022-05-10 19:24:10 +0300

    Messages changed

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 
 function choice1(select) {
     if (select.options[select.selectedIndex].text === 'Latvian') {
-        document.querySelector('.language').innerHTML = 'Latvian'
+        document.querySelector('.language').innerHTML = 'Sveiki'
     } else if (select.options[select.selectedIndex].text === 'Russian') {
-        document.querySelector('.language').innerHTML = 'Russian'
+        document.querySelector('.language').innerHTML = 'Privet'
     } else {
-        document.querySelector('.language').innerHTML = 'English'
+        document.querySelector('.language').innerHTML = 'Hello'
     }
 
 }
