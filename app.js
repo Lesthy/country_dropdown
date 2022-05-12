40f3f27a1f38db58cda99c296fb5dbbfc4134b5c
Lesthy <103683378+Lesthy@users.noreply.github.com>
2022-05-12 15:05:03 +0300

    Selection_update

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 
-function choice1(select) {
-    if (select.options[select.selectedIndex].text === 'Latvian') {
-        document.querySelector('.language').innerHTML = 'Sveiki'
-    } else if (select.options[select.selectedIndex].text === 'Russian') {
-        document.querySelector('.language').innerHTML = 'Privet'
-    } else {
-        document.querySelector('.language').innerHTML = 'Hello'
-    }
+  
+    const countries = ['UK', 'Latvia', 'Russia'],
+        select = document.getElementById('countries');
 
-}
+    for (country in countries) {
+
+        select.add(new Option(countries[country]));
+
+    };
+
+  
+    function display() {
+        var e = document.getElementById("countries");
+        var index = e.selectedIndex;
+        if (index == 0) {
+            document.querySelector('.language').innerHTML = 'Hello!'
+        }
+        else if (index == 1) {
+            document.querySelector('.language').innerHTML = 'Sveiki!'
+        }
+        else if (index == 2) {
+            document.querySelector('.language').innerHTML = 'Привет!'
+        }
+    }
